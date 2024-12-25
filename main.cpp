4c4edc0323fa0804e08b46fa0b9d5c1e914a4195
Bl4ckk3ys <ilchechnev@gmail.com>
2024-12-25 10:46:53 +0300

    z

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 // void foo2(){
 //     Polynom p;
-//     Monom a1 = Monom(4, 2, new int16_t[2]{2, 2});
+//     Monom a1 = Monom(4, 2, new int16_t[2]{3, 3});
 //     Monom a2 = Monom(1, 2, new int16_t[2]{1, 0});
 //     Monom a3 = Monom(1, 2, new int16_t[2]{0, 2});
 //     p.AppendMonom(a1);
@@ -21,22 +21,22 @@
 // }
 
 int main(){
-    Polynom p0;
-    Monom a1 = Monom(4, 1, new int16_t[1]{2});
-    Monom a2 = Monom(1, 1, new int16_t[1]{1});
-    Monom a3 = Monom(3, 1, new int16_t[1]{0});
-    p0.AppendMonom(a1);
-    p0.AppendMonom(a2);
-    p0.AppendMonom(a3);
-    Polynom p1;
-    Monom b1 = Monom(2, 1, new int16_t[1]{1});
-    Monom b2 = Monom(-1, 1, new int16_t[1]{0});
-    p1.AppendMonom(b1);
-    p1.AppendMonom(b2);
-    Polynom p2 = p0 / p1;
-    std::cout<<"res: ";
-    p2.Print();
-    std::cout << "\n";
+    // Polynom p0;
+    // Monom a1 = Monom(4, 2, new int16_t[2]{2,3});
+    // Monom a2 = Monom(1, 1, new int16_t[1]{1});
+    // Monom a3 = Monom(3, 1, new int16_t[1]{0});
+    // p0.AppendMonom(a1);
+    // p0.AppendMonom(a2);
+    // p0.AppendMonom(a3);
+    // Polynom p1;
+    // Monom b1 = Monom(2, 2, new int16_t[2]{1,1});
+    // Monom b2 = Monom(-1, 1, new int16_t[1]{0});
+    // p1.AppendMonom(b1);
+    // p1.AppendMonom(b2);
+    // Polynom p2 = p0 / p1;
+    // std::cout<<"res: ";
+    // p2.Print();
+    // std::cout << "\n";
     
     return 0;
 }
